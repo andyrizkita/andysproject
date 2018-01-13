f4af93f7a8e6a9fdf7cf9e44ca2812416738f844
Andi Rizkita Pratiwi <andy.rizkita@gmail.com>
2018-01-13 15:37:10 +0700

    Menambahkan Login Button

---
+++ app/src/main/java/com/arp/trainingday1/LoginLucu.java
@@ -0,0 +1,13 @@
+package com.arp.trainingday1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginLucu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginlucu_activity);
+    }
+}
